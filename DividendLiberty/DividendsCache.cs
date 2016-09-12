b9e61cd6e3e4d4fe9b5a0a79d7c1bda44e54fb8f
Steve Yi <steveyi32@gmail.com>
2016-09-11 21:44:02 -0500

    updated xml

--- DividendLiberty/DividendsCache.cs
+++ DividendLiberty/DividendsCache.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using MySql.Data.MySqlClient;
 using System.Configuration;
 using System.Data;
 using System.Windows.Forms;
